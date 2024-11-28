0fa6f8cf6fd68fd6b0d13697f6e37c7913a62f20
DESBOIS-RENAUDIN Meo <Meo.Desbois-Renaudin@bordeaux-inp.fr>
2024-11-28 20:03:57 +0000

    correcting

--- data_preparing/evaluate_function.py
+++ data_preparing/evaluate_function.py
@@ -3,6 +3,7 @@ from pandas import DataFrame, read_csv
 from sklearn.model_selection import train_test_split
 from matplotlib.pyplot import savefig, show, figure
 from dslabs_functions import plot_multibar_chart, evaluate_approach
+from balance import balance_dataset
 
 def split_and_clean_data(data: DataFrame, target: str = "class", test_size: float = 0.3) -> tuple:
     # Split the dataset into train and test (70% training, 30% testing)
@@ -10,30 +11,40 @@ def split_and_clean_data(data: DataFrame, target: str = "class", test_size: floa
     
     return train, test
 
-def main(dataset_path: str, target: str = "class"):
+def main(dataset_path: str, target: str = "class", balancing_method: str = "nothing", divide_by = 1) -> None:
     # Load the dataset
-    data: DataFrame = read_csv(dataset_path)[::100]
+    data: DataFrame = read_csv(dataset_path)
     
     # Split and clean the data (70% training, 30% testing)
     train, test = split_and_clean_data(data, target=target, test_size=0.3)
+
+    # Balance the dataset
+    if balancing_method != "nothing":
+        train = balance_dataset(train, balancing_method, target=target)
+
+    # Decimate the dataset
+    if divide_by != 1:
+        fraction = 1/divide_by
+        print(f"Taking a fraction of the dataset: {fraction}")
+        train = train.sample(frac=fraction, random_state=42)
     
     # Evaluate the approach
+    print("Evaluating the approach...")
     eval_results: dict[str, list] = evaluate_approach(train, test, target=target, metric="accuracy")
     
     # Plot and save the evaluation results
     figure()
     plot_multibar_chart(
-        ["NB", "KNN"], eval_results, title="Set 1 Evaluation after Filling Missing Values", percentage=True
+        ["NB", "KNN"], eval_results, title="Evaluation test", percentage=True
     )
-    savefig("data_preparing/images/set_1_evaluation_after_filling_missing_values.png")
+    savefig("data_preparing/images/evaluation_tests.png")
     show()
-
 
 
 # Example of running the main function with your dataset
 balancing_method = "undersampling"  # Change this to "nothing", "undersampling", "oversampling" or "SMOTE" if you want to use another method
 
-divide_by = 10  # Change this to 1 if you don't want to decimate the dataset
+divide_by = 1000  # Change this to 1 if you don't want to decimate the dataset
 dataset_path = "dataset/classification/no_mv_set_1.csv"  # Change this path to your actual dataset
 target = "LAW_CAT_CD"
 
