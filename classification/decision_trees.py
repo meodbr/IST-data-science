dfe3c5315ef44a8f12341d1315e305e943e12d5b
Ruben ROSA <ruben.rosa@grenoble-inp.org>
2024-12-02 19:18:52 +0000

    implementing code

---
+++ classification/decision_trees.py
@@ -0,0 +1,64 @@
+from typing import Literal
+from numpy import array, ndarray
+from matplotlib.pyplot import figure, savefig, show
+from sklearn.tree import DecisionTreeClassifier
+import sys
+import os
+# Ajouter la racine du projet au chemin de recherche
+racine = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(racine)
+from dslabs_functions import CLASS_EVAL_METRICS, DELTA_IMPROVE, read_train_test_from_files
+from dslabs_functions import plot_evaluation_results, plot_multiline_chart 
+
+
+def trees_study(
+        trnX: ndarray, trnY: array, tstX: ndarray, tstY: array, d_max: int=10, lag:int=2, metric='accuracy'
+        ) -> tuple:
+    criteria: list[Literal['entropy', 'gini']] = ['entropy', 'gini']
+    depths: list[int] = [i for i in range(2, d_max+1, lag)]
+
+    best_model: DecisionTreeClassifier | None = None
+    best_params: dict = {'name': 'DT', 'metric': metric, 'params': ()}
+    best_performance: float = 0.0
+
+    values: dict = {}
+    for c in criteria:
+        y_tst_values: list[float] = []
+        for d in depths:
+            clf = DecisionTreeClassifier(max_depth=d, criterion=c, min_impurity_decrease=0)
+            clf.fit(trnX, trnY)
+            prdY: array = clf.predict(tstX)
+            eval: float = CLASS_EVAL_METRICS[metric](tstY, prdY)
+            y_tst_values.append(eval)
+            if eval - best_performance > DELTA_IMPROVE:
+                best_performance = eval
+                best_params['params'] = (c, d)
+                best_model = clf
+            # print(f'DT {c} and d={d}')
+        values[c] = y_tst_values
+    print(f'DT best with {best_params['params'][0]} and d={best_params['params'][1]}')
+    plot_multiline_chart(depths, values, title=f'DT Models ({metric})', xlabel='d', ylabel=metric, percentage=True)
+
+    return best_model, best_params
+
+file_tag = 'set_1'
+train_filename = 'dataset/test_dataset_1.csv'
+test_filename = 'dataset/train_dataset_1.csv'
+target = 'JURISDICTION_CODE'
+eval_metric = 'accuracy'
+
+trnX, tstX, trnY, tstY, labels, vars = read_train_test_from_files(train_filename, test_filename, target)
+print(f'Train#={len(trnX)} Test#={len(tstX)}')
+print(f'Labels={labels}')
+
+figure()
+best_model, params = trees_study(trnX, trnY, tstX, tstY, d_max=25, metric=eval_metric)
+savefig(f'images_classification/{file_tag}_dt_{eval_metric}_study.png')
+show()
+
+prd_trn: array = best_model.predict(trnX)
+prd_tst: array = best_model.predict(tstX)
+figure()
+plot_evaluation_results(params, trnY, prd_trn, tstY, prd_tst, labels)
+savefig(f'images_classification/{file_tag}_dt_{params["name"]}_best_{params["metric"]}_eval.png')
+show()
